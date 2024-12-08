e96c755434fff357675fc0d150985d423b7f4597
Kylehinks97 <kyle.s.hinks@outlook.com>
2024-12-08 10:47:08 +0100

    Backend | TestSetup | Install and run first test

---
+++ backend/tests/Surface/BasicSurfaceTest.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace App\Tests\Surface;
+
+use App\Entity\Surface;
+use App\Repository\SurfaceRepository;
+use PHPUnit\Framework\TestCase;
+use function PHPUnit\Framework\assertEquals;
+
+class BasicSurfaceTest extends TestCase
+{
+    public function test_basic_surface()
+    {
+        assertEquals(0, 0);
+    }
+}
